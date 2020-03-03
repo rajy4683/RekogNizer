3637fb94d3024f073fe1bec0a2da537b474b528a
rajy4683 <st.hazard@gmail.com>
2020-03-03 18:29:08 +0530

    RF and output sizes updated

--- basemodelclass.py
+++ basemodelclass.py
@@ -1,5 +1,5 @@
 """
-     All model class definitions for MNIST
+     All model class definitions for RekogNizer library
 """
 
 from __future__ import print_function
@@ -9,6 +9,106 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
+
+"""
+     Model for CIFAR used in S7EVA4 assignment
+"""
+
+class CIFARModelDepthDilate(nn.Module):
+    def __init__(self,dropout=0.1):
+        super(CIFARModelDepthDilate, self).__init__()
+        self.layer1_channels = 32
+        self.dropout_val = dropout
+        self.bias = False
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, self.layer1_channels, 3, padding=1, stride=1,bias=self.bias), #Input: (3,32,32)|Output (32,32,32)|RF=3
+            #nn.Conv2d(3,self.layer1_channels,1,1,0,1,1,bias=bias),
+            nn.BatchNorm2d(self.layer1_channels),
+            nn.ReLU(),
+            nn.Dropout(self.dropout_val),
+            nn.Conv2d(self.layer1_channels, self.layer1_channels, 3, padding=1, stride=1,bias=self.bias,groups=self.layer1_channels), 
+            nn.Conv2d(self.layer1_channels,self.layer1_channels,1,1,0,1,1,bias=self.bias),  #Input: (32,32,32)|Output (32,32,32)|RF=5    
+            nn.BatchNorm2d(self.layer1_channels),
+            nn.ReLU(),
+            # nn.Conv2d(self.layer1_channels, self.layer1_channels, 3, padding=1, bias=self.bias),            
+            # nn.BatchNorm2d(self.layer1_channels),
+            # nn.ReLU(),
+            nn.MaxPool2d(2, 2),   #Input: (32,32,32)|Output (32,16,16)|RF=6
+            nn.Dropout(self.dropout_val))
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(self.layer1_channels, self.layer1_channels*2, 3, padding=1, stride=1,bias=self.bias), #Input: (32,16,16)|Output (64,16,16)|RF=10
+            #nn.Conv2d(self.layer1_channels,self.layer1_channels*2,1,1,0,1,1,bias=self.bias),
+            nn.BatchNorm2d(self.layer1_channels*2),
+            nn.ReLU(),
+            nn.Dropout(self.dropout_val),
+            nn.Conv2d(self.layer1_channels*2, self.layer1_channels*2, 3, padding=1, stride=1,bias=self.bias, groups=self.layer1_channels*2),
+            nn.Conv2d(self.layer1_channels*2,self.layer1_channels*2,1,1,0,1,1,bias=self.bias),      #Input: (64,16,16)|Output (64,16,16)|RF=14
+            nn.BatchNorm2d(self.layer1_channels*2),
+            nn.ReLU(),
+            # nn.Conv2d(self.layer1_channels*2, self.layer1_channels*2, 3, padding=1, bias=self.bias),            
+            # nn.BatchNorm2d(self.layer1_channels*2),
+            # nn.ReLU(),
+            nn.MaxPool2d(2, 2), #Input: (64,16,16)|Output (64,8,8)|RF=16
+            nn.Dropout(self.dropout_val))
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(self.layer1_channels*2, self.layer1_channels*4, 3, padding=1, stride=1,bias=self.bias,dilation=2), #Input: (64,8,8)|Output (128,8,8)|RF=32,
+            #nn.Conv2d(self.layer1_channels*2,self.layer1_channels*4,1,1,0,1,1,bias=self.bias),       
+            nn.BatchNorm2d(self.layer1_channels*4),
+            nn.ReLU(),
+            nn.Dropout(self.dropout_val),
+            nn.Conv2d(self.layer1_channels*4, self.layer1_channels*4, 3, padding=1, stride=1,bias=self.bias, groups=self.layer1_channels*4, dilation=2),
+            nn.Conv2d(self.layer1_channels*4, self.layer1_channels*4,1,1,0,1,1,bias=self.bias),#Input: (128,8,8)|Output (128,8,8)|RF=48
+            nn.BatchNorm2d(self.layer1_channels*4),
+            nn.ReLU(),
+            # nn.Conv2d(self.layer1_channels*4, self.layer1_channels*4, 3, padding=1, bias=self.bias),            
+            # nn.BatchNorm2d(self.layer1_channels*4),
+            # nn.ReLU(),
+            nn.MaxPool2d(2, 2),  #Input: (128,8,8)|Output (128,4,4)|RF=52
+            nn.Dropout(self.dropout_val))
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(self.layer1_channels*4, self.layer1_channels*8, 3, padding=1, stride=1,bias=self.bias),#Input: (128,4,4)|Output (256,4,4)|RF=68            
+            nn.BatchNorm2d(self.layer1_channels*8),
+            nn.ReLU(),
+            nn.Dropout(self.dropout_val),
+            nn.Conv2d(self.layer1_channels*8, self.layer1_channels*8, 3, padding=1, stride=1,bias=self.bias, groups=self.layer1_channels),
+            nn.Conv2d(self.layer1_channels*8, self.layer1_channels*8,1,1,0,1,1,bias=self.bias), #Input: (256,4,4)|Output (256,4,4)|RF=84
+            nn.BatchNorm2d(self.layer1_channels*8),
+            nn.ReLU(),
+            # nn.Conv2d(self.layer1_channels*4, self.layer1_channels*4, 3, padding=1, bias=self.bias),            
+            # nn.BatchNorm2d(self.layer1_channels*8),
+            # nn.ReLU(),
+            #nn.MaxPool2d(2, 2),
+            nn.Dropout(self.dropout_val))
+        
+        self.gap_linear = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Conv2d(self.layer1_channels*8, 10, 1, bias=self.bias)
+        )
+
+    def forward(self, x):
+        # x = self.pool(F.relu(self.conv1(x)))
+        # x = self.pool(F.relu(self.conv2(x)))
+        # x = x.view(-1, 16 * 5 * 5)
+        # x = F.relu(self.fc1(x))
+        # x = F.relu(self.fc2(x))
+        # x = self.fc3(x)
+
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        #x = x.view(x.size(0), -1)
+        x = self.gap_linear(x)
+        x = x.view(-1, 10)
+        x = F.log_softmax(x, dim=1)
+
+        return x
+
+
 
 class MNISTDigitBuilder(nn.Module):
     def __init__(self, dropout=0.1):
@@ -108,99 +208,6 @@ class BareCIFAR(nn.Module):
         x = self.fc3(x)
         return x
 
-class CIFARModelDepthDilate(nn.Module):
-    def __init__(self,dropout=0.1):
-        super(CIFARModelDepthDilate, self).__init__()
-        self.layer1_channels = 32
-        self.dropout_val = dropout
-        self.bias = False
-
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, self.layer1_channels, 3, padding=1, stride=1,bias=self.bias),
-            #nn.Conv2d(3,self.layer1_channels,1,1,0,1,1,bias=bias),
-            nn.BatchNorm2d(self.layer1_channels),
-            nn.ReLU(),
-            nn.Dropout(self.dropout_val),
-            nn.Conv2d(self.layer1_channels, self.layer1_channels, 3, padding=1, stride=1,bias=self.bias,groups=self.layer1_channels),
-            nn.Conv2d(self.layer1_channels,self.layer1_channels,1,1,0,1,1,bias=self.bias),      
-            nn.BatchNorm2d(self.layer1_channels),
-            nn.ReLU(),
-            # nn.Conv2d(self.layer1_channels, self.layer1_channels, 3, padding=1, bias=self.bias),            
-            # nn.BatchNorm2d(self.layer1_channels),
-            # nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Dropout(self.dropout_val))
-
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(self.layer1_channels, self.layer1_channels*2, 3, padding=1, stride=1,bias=self.bias), #groups=self.layer1_channels),
-            #nn.Conv2d(self.layer1_channels,self.layer1_channels*2,1,1,0,1,1,bias=self.bias),
-            nn.BatchNorm2d(self.layer1_channels*2),
-            nn.ReLU(),
-            nn.Dropout(self.dropout_val),
-            nn.Conv2d(self.layer1_channels*2, self.layer1_channels*2, 3, padding=1, stride=1,bias=self.bias, groups=self.layer1_channels*2),
-            nn.Conv2d(self.layer1_channels*2,self.layer1_channels*2,1,1,0,1,1,bias=self.bias),      
-            nn.BatchNorm2d(self.layer1_channels*2),
-            nn.ReLU(),
-            # nn.Conv2d(self.layer1_channels*2, self.layer1_channels*2, 3, padding=1, bias=self.bias),            
-            # nn.BatchNorm2d(self.layer1_channels*2),
-            # nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Dropout(self.dropout_val))
-
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(self.layer1_channels*2, self.layer1_channels*4, 3, padding=1, stride=1,bias=self.bias,dilation=2), #groups=self.layer1_channels*2),
-            #nn.Conv2d(self.layer1_channels*2,self.layer1_channels*4,1,1,0,1,1,bias=self.bias),       
-            nn.BatchNorm2d(self.layer1_channels*4),
-            nn.ReLU(),
-            nn.Dropout(self.dropout_val),
-            nn.Conv2d(self.layer1_channels*4, self.layer1_channels*4, 3, padding=1, stride=1,bias=self.bias, groups=self.layer1_channels*4, dilation=2),
-            nn.Conv2d(self.layer1_channels*4, self.layer1_channels*4,1,1,0,1,1,bias=self.bias),
-            nn.BatchNorm2d(self.layer1_channels*4),
-            nn.ReLU(),
-            # nn.Conv2d(self.layer1_channels*4, self.layer1_channels*4, 3, padding=1, bias=self.bias),            
-            # nn.BatchNorm2d(self.layer1_channels*4),
-            # nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Dropout(self.dropout_val))
-
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(self.layer1_channels*4, self.layer1_channels*8, 3, padding=1, stride=1,bias=self.bias),#, groups=self.layer1_channels),            
-            nn.BatchNorm2d(self.layer1_channels*8),
-            nn.ReLU(),
-            nn.Dropout(self.dropout_val),
-            nn.Conv2d(self.layer1_channels*8, self.layer1_channels*8, 3, padding=1, stride=1,bias=self.bias, groups=self.layer1_channels),
-            nn.Conv2d(self.layer1_channels*8, self.layer1_channels*8,1,1,0,1,1,bias=self.bias),
-            nn.BatchNorm2d(self.layer1_channels*8),
-            nn.ReLU(),
-            # nn.Conv2d(self.layer1_channels*4, self.layer1_channels*4, 3, padding=1, bias=self.bias),            
-            # nn.BatchNorm2d(self.layer1_channels*8),
-            # nn.ReLU(),
-            #nn.MaxPool2d(2, 2),
-            nn.Dropout(self.dropout_val))
-        
-        self.gap_linear = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1,1)),
-            nn.Conv2d(self.layer1_channels*8, 10, 1, bias=self.bias)
-        )
-
-    def forward(self, x):
-        # x = self.pool(F.relu(self.conv1(x)))
-        # x = self.pool(F.relu(self.conv2(x)))
-        # x = x.view(-1, 16 * 5 * 5)
-        # x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        # x = self.fc3(x)
-
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        #x = x.view(x.size(0), -1)
-        x = self.gap_linear(x)
-        x = x.view(-1, 10)
-        x = F.log_softmax(x, dim=1)
-
-        return x
 
 class CIFARModel(nn.Module):
     def __init__(self):
