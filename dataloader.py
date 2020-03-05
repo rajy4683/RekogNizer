ffab07ef26b2ef5aeee8b2eaf4f6553f7ad6d642
rajy4683 <st.hazard@gmail.com>
2020-03-05 21:51:38 +0530

    moved transforms and datasets to trainer scripts

--- dataloader.py
+++ dataloader.py
@@ -3,20 +3,12 @@ import torchvision
 import torchvision.transforms as transforms
 from RekogNizer.hyperparams import *
 
-torch.manual_seed(hyperparameter_defaults['seed'])
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=hyperparameter_defaults['batch_size'],
-                                          shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=hyperparameter_defaults['batch_size'],
-                                         shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+def get_dataloader(dataset, batch_size, shuffle=True, num_workers=2):
+    return torch.utils.data.DataLoader(dataset, batch_size,
+                                         shuffle=shuffle, num_workers=num_workers)
